b31cddb9ee2e8234acf674375363d3272951edb2
Bùi Xuân Hải <buixuanhai.it@gmail.com>
2022-01-03 07:52:13 +0700

    update matrix

--- src/RotateMatrix.js
+++ src/RotateMatrix.js
@@ -1,6 +1,5 @@
-import { useEffect, useMemo, useState } from "react";
-import { FcNext, FcPrevious } from "react-icons/fc";
-import { GrChapterNext, GrChapterPrevious } from "react-icons/gr";
+import { useMemo, useState } from "react";
+import { Steps, Matrix } from "./components";
 
 function rotate(originalMatrix, onStep) {
   const matrix = JSON.parse(JSON.stringify(originalMatrix));
@@ -42,105 +41,6 @@ function initializeMatrix(size) {
   return matrix;
 }
 
-function Steps({
-  current,
-  length,
-  onSetStep,
-  goToNext,
-  goToPrev,
-  goToEnd,
-  goToBegin
-}) {
-  const steps = useMemo(() => {
-    const result = [];
-    for (let i = 0; i < length; i++) {
-      result.push(i);
-    }
-    return result;
-  }, [length]);
-  return (
-    <div>
-      <div style={{ marginBottom: 10 }}>
-        Step {current} / {length - 1}
-      </div>
-      <div style={{ marginBottom: 10 }}>
-        <button onClick={goToBegin}>
-          <GrChapterPrevious />
-        </button>
-        <button onClick={goToPrev}>
-          <FcPrevious />
-        </button>
-        <button onClick={goToNext}>
-          <FcNext />
-        </button>
-        <button onClick={goToEnd}>
-          <GrChapterNext />
-        </button>
-      </div>
-      <div style={{ height: 100, width: 60, overflowY: "scroll" }}>
-        {steps.map((i, index) => (
-          <div
-            key={index}
-            onClick={() => onSetStep(i)}
-            style={{
-              cursor: "pointer",
-              backgroundColor: i === current ? "azure" : ""
-            }}
-          >
-            {i}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-function Cell({ children }) {
-  const [backgroundColor, setBackgroundColor] = useState("");
-  useEffect(() => {
-    setBackgroundColor("#FFD39A");
-    setTimeout(() => {
-      setBackgroundColor("");
-    }, 500);
-  }, [children]);
-  return (
-    <div
-      style={{
-        border: "1px solid",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor
-      }}
-    >
-      {children}
-    </div>
-  );
-}
-function Matrix({ matrix, title }) {
-  const length = matrix.length;
-
-  return (
-    <div>
-      <p style={{ textAlign: "center" }}>{title}</p>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: `repeat(${length}, 40px)`,
-          gridTemplateRows: `repeat(${length}, 40px)`,
-          marginRight: 10
-        }}
-      >
-        {matrix.map((row, index) => {
-          return row.map((cell, colIndex) => (
-            <Cell key={`${index}-${colIndex}`}>{cell}</Cell>
-          ));
-        })}
-      </div>
-    </div>
-  );
-}
-
 export default function RotateMatrix() {
   const [step, setStep] = useState(0);
   const [size, setSize] = useState(3);
@@ -151,6 +51,10 @@ export default function RotateMatrix() {
 
   return (
     <div>
+      <div style={{ display: "flex", marginBottom: 10 }}>
+        <Matrix title="Original" matrix={state[0]} />
+        <Matrix title="Transformed" matrix={state[step]} />
+      </div>
       <div>
         <div style={{ marginBottom: 10 }}>
           <label style={{ paddingRight: 10 }}>Matrix size</label>
@@ -189,10 +93,6 @@ export default function RotateMatrix() {
           length={state.length}
           onSetStep={(i) => setStep(i)}
         />
-      </div>
-      <div style={{ display: "flex", marginBottom: 10 }}>
-        <Matrix title="Original" matrix={state[0]} />
-        <Matrix title="Transformed" matrix={state[step]} />
       </div>
     </div>
   );
